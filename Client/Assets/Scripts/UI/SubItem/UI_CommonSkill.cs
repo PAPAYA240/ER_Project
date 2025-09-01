6aacd9c5ae25a7c7cbc7f217b8c6aaf6bee66e0a
kugiri <kyschaos@naver.com>
2025-09-01 17:36:04 +0900

    [UI] 프로필, 인터페이스
    플레이어 프로필 데이터 추가
    플레이어 인터페이스 프리팹 구현
    일반 스킬 레벨 세팅
    쿨타임 기능 구현

---
+++ Client/Assets/Scripts/UI/SubItem/UI_CommonSkill.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Playables;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CommonSkill : UI_Scene
+{
+    enum Buttons
+    {
+        SkillButton
+    }
+
+    enum Texts
+    {
+        StaminaCost,
+        CooldownTimerText,
+        MapingKey
+    }
+
+    enum Images
+    {
+        Level_1,
+        Level_2,
+        Level_3,
+        Level_4,
+        Level_5,
+        CooldownFill
+    }
+
+    enum GameObjects
+    {
+        Stamina,
+        CooldownTimer
+    }
+
+    enum ColorEnum
+    {
+        Yellow,
+        Gray
+    }
+
+    const string _yellow = "#B89249";
+    const string _gray = "#505050";
+
+    int _skillLevel = 1;
+
+    const int _cooldownTimer = (int)GameObjects.CooldownTimer;
+    const int _stamina = (int)GameObjects.Stamina;
+
+    //Temp
+    float _remainCool = 0;
+    float _maxCool = 10.0f;
+
+    public override void Init()
+    {
+        base.Init();
+
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(Texts));
+        Bind<Image>(typeof(Images));
+        Bind<GameObject>(typeof(GameObjects));
+
+        GetObject(_stamina).gameObject.SetActive(false);
+        GetObject(_cooldownTimer).gameObject.SetActive(false);
+
+        SetSkillLevel(3);
+        UseSkill();
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        //temp
+        //쿨다운타이머가 활성화 되어 있으면 셋 쿨다운을 호출해서 쿨타임을 지속적으로 갱신
+        //TODO 쿨타임 이미지 돌아가는거 해야됨.
+        if(GetObject(_cooldownTimer).activeSelf && _remainCool > 0.0f)
+        {
+            _remainCool = Math.Max(0.0f, _remainCool - Time.deltaTime);
+
+            if( _remainCool > 0.0f )
+            {
+                // 쿨타임이 남아있을때
+                GetImage((int)Images.CooldownFill).fillAmount = _remainCool / _maxCool;
+                SetCoolDown(_remainCool);
+            }
+            else
+            {
+                GetObject(_cooldownTimer).SetActive(false);
+            }
+        }
+
+        
+    }
+
+    void SetSkillLevel(int level)
+    {
+        _skillLevel = level;
+
+        for(int i = 1; i <= _skillLevel; ++i) 
+            ChangeColor(i, ColorEnum.Yellow);
+
+        for (int i = _skillLevel + 1; i <= 5; ++i)
+            ChangeColor(i, ColorEnum.Gray);
+    }
+    
+    void SkillLevelUp()
+    {
+        SetSkillLevel(_skillLevel + 1);
+    }
+
+    void ChangeColor(int level, ColorEnum color)
+    {
+        Color destColor = Color.black;
+
+        if(color == ColorEnum.Yellow)
+        {
+            if (!ColorUtility.TryParseHtmlString(_yellow, out destColor))
+            {
+                Debug.Log($"Failed to TryParseHtmlString : {_yellow}");
+                return;
+            }
+        }
+        else if(color == ColorEnum.Gray)
+        {
+            if (!ColorUtility.TryParseHtmlString(_gray, out destColor))
+            {
+                Debug.Log($"Failed to TryParseHtmlString : {_gray}");
+                return;
+            }
+        }
+
+        GetImage(level - 1).color = destColor;
+    }
+
+    void UseSkill()
+    {
+        //스킬을 사용하면 타이머를 활성화
+        //활성화 되면 스킬이 어두워지고 쿨타임이 표시됨.
+        GetObject(_cooldownTimer).SetActive(true);
+        //temp
+        _remainCool = 10.0f;
+        //SetCoolDown(0.4f);
+    }
+
+    void SetCoolDown(float remainCooldown)
+    {
+        string text = "";
+
+        if (remainCooldown > 1.0f)
+        {
+            text = remainCooldown.ToString("F0");
+        }
+        else
+        {
+            text = remainCooldown.ToString("F1");
+        }
+
+        TextMeshProUGUI textObject = GetText((int)Texts.CooldownTimerText);
+        if (textObject != null)
+            textObject.text = text;
+    }
+}
