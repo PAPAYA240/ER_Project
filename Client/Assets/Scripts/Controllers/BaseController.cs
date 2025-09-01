8020d54c367d8d2ef9dac3d3e313ff93627caade
ahnjh97 <ahnjh1997@naver.com>
2025-09-01 14:52:44 +0900

    PosInfo, RotInfo 서버 동기화

--- Client/Assets/Scripts/Controllers/BaseController.cs
+++ Client/Assets/Scripts/Controllers/BaseController.cs
@@ -42,6 +42,7 @@ public class BaseController : MonoBehaviour
     protected bool _updated = false;
 
     PositionInfo _positionInfo = new PositionInfo();
+    RotationInfo _rotationInfo = new RotationInfo();
     public PositionInfo PosInfo
     {
         get { return _positionInfo; }
@@ -50,38 +51,52 @@ public class BaseController : MonoBehaviour
             if (_positionInfo.Equals(value))
                 return;
 
-            CellPos = new Vector3Int(value.PosX, value.PosY, 0);
+            CellPos = new Vector3(value.PosX, value.PosY, value.PosZ);
             State = value.State;
-            Dir = value.MoveDir;
+        }
+    }
+
+    public RotationInfo RotInfo
+    {
+        get { return _rotationInfo; }
+        set
+        {
+            if (_rotationInfo.Equals(value))
+                return;
+
+            _rotationInfo.Qx = value.Qx;
+            _rotationInfo.Qy = value.Qy;
+            _rotationInfo.Qz = value.Qz;
+            _rotationInfo.Qw = value.Qw;
         }
     }
 
     public void SyncPos()
     {
-        //Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
-        //transform.position = destPos;
+        transform.position = CellPos;
+        transform.rotation = RotInfo;
     }
 
-    public Vector3Int CellPos
+    public Vector3 CellPos
     {
         get
         {
-            return new Vector3Int(PosInfo.PosX, PosInfo.PosY, 0);
+            return new Vector3(PosInfo.PosX, PosInfo.PosY, PosInfo.PosZ);
         }
 
         set
         {
-            if (PosInfo.PosX == value.x && PosInfo.PosY == value.y)
+            if (PosInfo.PosX == value.x && PosInfo.PosY == value.y && PosInfo.PosZ == value.z)
                 return;
 
             PosInfo.PosX = value.x;
             PosInfo.PosY = value.y;
+            PosInfo.PosZ = value.z;
             _updated = true;
         }
     }
 
     protected Animator _animator;
-    protected SpriteRenderer _sprite;
 
     public virtual CreatureState State
     {
@@ -97,126 +112,22 @@ public class BaseController : MonoBehaviour
         }
     }
 
-    public MoveDir Dir
-    {
-        get { return PosInfo.MoveDir; }
-        set
-        {
-            if (PosInfo.MoveDir == value)
-                return;
-
-            PosInfo.MoveDir = value;
-
-            UpdateAnimation();
-            _updated = true;
-        }
-    }
-
-    public MoveDir GetDirFromVec(Vector3Int dir)
-    {
-        if (dir.x > 0)
-            return MoveDir.Right;
-        else if (dir.x < 0)
-            return MoveDir.Left;
-        else if (dir.y > 0)
-            return MoveDir.Up;
-        else
-            return MoveDir.Down;
-    }
-
-    public Vector3Int GetFrontCellPos()
-    {
-        Vector3Int cellPos = CellPos;
-
-        switch (Dir)
-        {
-            case MoveDir.Up:
-                cellPos += Vector3Int.up;
-                break;
-            case MoveDir.Down:
-                cellPos += Vector3Int.down;
-                break;
-            case MoveDir.Left:
-                cellPos += Vector3Int.left;
-                break;
-            case MoveDir.Right:
-                cellPos += Vector3Int.right;
-                break;
-        }
-
-        return cellPos;
-    }
-
     protected virtual void UpdateAnimation()
     {
-        if (_animator == null || _sprite == null)
+        if (_animator == null)
             return;
 
         if (State == CreatureState.Idle)
         {
-            switch (Dir)
-            {
-                case MoveDir.Up:
-                    _animator.Play("IDLE_BACK");
-                    _sprite.flipX = false;
-                    break;
-                case MoveDir.Down:
-                    _animator.Play("IDLE_FRONT");
-                    _sprite.flipX = false;
-                    break;
-                case MoveDir.Left:
-                    _animator.Play("IDLE_RIGHT");
-                    _sprite.flipX = true;
-                    break;
-                case MoveDir.Right:
-                    _animator.Play("IDLE_RIGHT");
-                    _sprite.flipX = false;
-                    break;
-            }
+
         }
         else if (State == CreatureState.Moving)
         {
-            switch (Dir)
-            {
-                case MoveDir.Up:
-                    _animator.Play("WALK_BACK");
-                    _sprite.flipX = false;
-                    break;
-                case MoveDir.Down:
-                    _animator.Play("WALK_FRONT");
-                    _sprite.flipX = false;
-                    break;
-                case MoveDir.Left:
-                    _animator.Play("WALK_RIGHT");
-                    _sprite.flipX = true;
-                    break;
-                case MoveDir.Right:
-                    _animator.Play("WALK_RIGHT");
-                    _sprite.flipX = false;
-                    break;
-            }
+
         }
         else if (State == CreatureState.Skill)
         {
-            switch (Dir)
-            {
-                case MoveDir.Up:
-                    _animator.Play("ATTACK_BACK");
-                    _sprite.flipX = false;
-                    break;
-                case MoveDir.Down:
-                    _animator.Play("ATTACK_FRONT");
-                    _sprite.flipX = false;
-                    break;
-                case MoveDir.Left:
-                    _animator.Play("ATTACK_RIGHT");
-                    _sprite.flipX = true;
-                    break;
-                case MoveDir.Right:
-                    _animator.Play("ATTACK_RIGHT");
-                    _sprite.flipX = false;
-                    break;
-            }
+
         }
         else
         {
@@ -237,10 +148,8 @@ public class BaseController : MonoBehaviour
     protected virtual void Init()
     {
         _animator = GetComponent<Animator>();
-        _sprite = GetComponent<SpriteRenderer>();
-        //Vector3 pos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
-        //transform.position = pos;
-
+        
+        SyncPos();
         UpdateAnimation();
     }
 
--- Server/Server/Game/Room/GameRoom.cs
+++ Server/Server/Game/Room/GameRoom.cs
@@ -16,12 +16,8 @@ namespace Server.Game
         Dictionary<int, Monster> _monsters = new Dictionary<int, Monster>();
         Dictionary<int, Projectile> _projectiles = new Dictionary<int, Projectile>();
 
-        public Map Map { get; private set; } = new Map();
-
         public void Init(int mapId)
         {
-            Map.LoadMap(mapId);
-
             // TEMP
             //Monster monster = ObjectManager.Instance.Add<Monster>();
             //monster.CellPos = new Vector2Int(5, 5);
@@ -55,8 +51,6 @@ namespace Server.Game
                 _players.Add(gameObject.Id, player);
                 player.Room = this;
 
-                Map.ApplyMove(player, new Vector2Int(player.CellPos.x, player.CellPos.y));
-
                 // 본인한테 정보 전송
                 {
                     S_EnterGame enterPacket = new S_EnterGame();
@@ -84,8 +78,6 @@ namespace Server.Game
                 Monster monster = gameObject as Monster;
                 _monsters.Add(gameObject.Id, monster); 
                 monster.Room = this;
-
-                Map.ApplyMove(monster, new Vector2Int(monster.CellPos.x, monster.CellPos.y));
             }
             else if (type == GameObjectType.Projectile)
             {
@@ -116,7 +108,6 @@ namespace Server.Game
                 if (_players.Remove(objectId, out player) == false)
                     return;
 
-                Map.ApplyLeave(player);
                 player.Room = null;
 
                 // 본인한테 정보 전송
@@ -130,8 +121,7 @@ namespace Server.Game
                 Monster monster = null;
                 if (_monsters.Remove(objectId, out monster) == false)
                     return;
-               
-                Map.ApplyLeave(monster);
+                
                 monster.Room = null;
             }
             else if (type == GameObjectType.Projectile)
@@ -163,24 +153,19 @@ namespace Server.Game
             // todo : 검증
 
             // 일단 서버에서 좌표 이동
-            PositionInfo movePosInfo = movePacket.PosInfo;
-            ObjectInfo info = player.Info;
-
-            // 다른 좌표로 이동할 경우, 갈 수 있는지 체크
-            if(movePosInfo.PosX != info.PosInfo.PosX || movePosInfo.PosY != info.PosInfo.PosY)
-            {
-                if (Map.CanGo(new Vector2Int(movePosInfo.PosX, movePosInfo.PosY)) == false)
-                    return;
-            }
-
-            info.PosInfo.State = movePosInfo.State;
-            info.PosInfo.MoveDir = movePosInfo.MoveDir;
-            Map.ApplyMove(player, new Vector2Int(movePosInfo.PosX, movePosInfo.PosY));
+            player.Info.PosInfo.PosX = movePacket.PosInfo.PosX;
+            player.Info.PosInfo.PosY = movePacket.PosInfo.PosY;
+            player.Info.PosInfo.PosZ = movePacket.PosInfo.PosZ;
+            player.Info.RotInfo.Qx = movePacket.RotInfo.Qx;
+            player.Info.RotInfo.Qy = movePacket.RotInfo.Qy;
+            player.Info.RotInfo.Qz = movePacket.RotInfo.Qz;
+            player.Info.RotInfo.Qw = movePacket.RotInfo.Qw;
 
             // 다른 플레이어한테도 알려준다
             S_Move resMovePacket = new S_Move();
             resMovePacket.ObjectId = player.Info.ObjectId;
             resMovePacket.PosInfo = movePacket.PosInfo;
+            resMovePacket.RotInfo = movePacket.RotInfo;
 
             Broadcast(resMovePacket);
         }
@@ -210,13 +195,13 @@ namespace Server.Game
             {
                 case SkillType.SkillAuto:
                     {
-                        // 데미지 판정
-                        Vector2Int skillPos = player.GetFrontCellPos(info.PosInfo.MoveDir);
-                        GameObject target = Map.Find(skillPos);
-                        if (target != null)
-                        {
-                            Console.WriteLine("Hit GameObject!");
-                        }
+                        //// 데미지 판정
+                        //Vector2Int skillPos = player.GetFrontCellPos(info.PosInfo.MoveDir);
+                        //GameObject target = Map.Find(skillPos);
+                        //if (target != null)
+                        //{
+                        //    Console.WriteLine("Hit GameObject!");
+                        //}
                     }
                     break;
                 case SkillType.SkillProjectile:
@@ -228,7 +213,6 @@ namespace Server.Game
                         arrow.Owner = player;
                         arrow.Data = skillData;
                         arrow.PosInfo.State = CreatureState.Moving;
-                        arrow.PosInfo.MoveDir = player.PosInfo.MoveDir;
                         arrow.PosInfo.PosX = player.PosInfo.PosX;
                         arrow.PosInfo.PosY = player.PosInfo.PosY;
                         arrow.Speed = skillData.projectile.speed;
