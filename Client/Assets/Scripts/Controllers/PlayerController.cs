8093350a63534095aeee9061ef5707016a821b47
kugiri <kyschaos@naver.com>
2025-09-08 18:36:05 +0900

    [UI] 미니맵
    미니맵 프리팹 구현
    미니맵 전장의 안개 구현(렌더 텍스쳐, 전장의 안개 스크립트, 셰이더 + 머터리얼, 전장의 안개 카메라)
    셰이더에서 렌더 텍스쳐 가운데 1800 * 1600 만 사용하도록 수정
    PlayerHUD에 미니맵 프리팹 추가

--- Client/Assets/Scripts/Controllers/PlayerController.cs
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -15,11 +15,24 @@ public class PlayerController : CreatureController
 
     protected Dictionary<string, SkillBase> _skills = new Dictionary<string, SkillBase>();
 
+    //Fog
+    //private GameObject _fogOfWarVisionGo;
+    private FogOfWarVision _fogOfWarVision;
+
     protected override void Init()
 	{
 		base.Init();
 		MakeSkillDict();
-        ObjectType = Define.Object.OtherPlayer; 
+        ObjectType = Define.Object.OtherPlayer;
+
+        //Fog
+        //_fogOfWarVisionGo = new GameObject("FogOfWar");
+        //_fogOfWarVisionGo.transform.SetParent(gameObject.transform);
+        //_fogOfWarVision = _fogOfWarVisionGo.GetOrAddComponent<FogOfWarVision>();
+        //_fogOfWarVisionGo.layer = LayerMask.NameToLayer("Fog");
+
+        _fogOfWarVision = gameObject.GetOrAddComponent<FogOfWarVision>();
+        gameObject.layer = LayerMask.NameToLayer("Fog");
     }
 
 	protected override void UpdateController()
