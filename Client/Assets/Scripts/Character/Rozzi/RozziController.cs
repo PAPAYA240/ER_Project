aff8eb3602fed3cd51dd0f01df344d8142f61d27
yeonjin <xxyeon17@gmail.com>
2025-09-12 12:03:41 +0900

    몬스터 타겟팅해서 이동
    - 맵 클릭하면 이동가능한 위치까지 이동
    - 몬스터 클릭하면 평타 사거리만큼 떨어진 곳으로 이동
    - 이미 평타 사거리 내라면 이동x

--- Client/Assets/Scripts/Character/Rozzi/RozziController.cs
+++ Client/Assets/Scripts/Character/Rozzi/RozziController.cs
@@ -12,7 +12,13 @@ using UnityEngine.Playables;
 
 public class RozziController : MyPlayerController
 {
-    private NavMeshAgent _agent;
+    protected NavMeshAgent _agent;
+    //protected LayerMask _targetLayer = LayerMask.GetMask("")
+    protected bool _isTargetOn;
+    protected GameObject _targetMonster;
+
+    // TEMP
+    protected float _attackRange;
 
     protected override void Init()
     {
@@ -22,6 +28,8 @@ public class RozziController : MyPlayerController
         _agent.acceleration = 999;
         _agent.angularSpeed = 720;
         _agent.stoppingDistance = 0.1f;
+
+        _attackRange = 5.0f;
     }
 
     protected override void UpdateKeyInput()
@@ -50,6 +58,8 @@ public class RozziController : MyPlayerController
         base.UpdateIdle();
     }
 
+    // 목적지까지 실제로 움직임
+    // 목적지까지 도착했으면 Moving 상태 종료 -> Idle
     protected override void UpdateMoving()
     {
         //base.UpdateMoving();
@@ -77,21 +87,68 @@ public class RozziController : MyPlayerController
                 RotInfo = transform.rotation;
                 CheckUpdatedFlag();
             }
+
+            if(_isTargetOn)
+                LookAtTarget();
         }
     }
 
+    protected void LookAtTarget()
+    {
+        Vector3 lookDir = (_targetMonster.transform.position - transform.position).normalized;
+        lookDir.y = 0f;
+
+        if(lookDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDir), Time.deltaTime * 10f);
+        }
+    }
+
+    // 마우스 우클릭이 눌렸을 경우 유효한 곳이 클릭 되었다면 해당 위치를 목적지로 설정
+    // 몬스터 클릭 시 평타 사거리만큼 떨어진 곳으로 설정
+    // Moving 상태로 변경
     protected override void GetMouseInput()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        bool raycastHit = Physics.Raycast(ray, out hit, 1000.0f, _mask);
-
         if (Input.GetMouseButton(1))
         {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool raycastHit = Physics.Raycast(ray, out hit, 1000.0f);
+            
             if (raycastHit)
             {
-                NavMeshHit navHit;
-                if(NavMesh.SamplePosition(hit.point, out navHit, 2.0f, NavMesh.AllAreas))
+                GameObject targetObject = hit.collider.gameObject;
+                Vector3 targetPos;
+
+                // 마우스와 충돌한 물체가
+                // 몬스터인 경우
+                if (targetObject.layer != LayerMask.NameToLayer("Map"))
+                {
+                    _isTargetOn = true;
+                    _targetMonster = targetObject;
+
+                    Vector3 monsterPos = targetObject.transform.position;
+                    Vector3 dir = (monsterPos - transform.position).normalized;
+
+                    float distance = Vector3.Distance(transform.position, monsterPos);
+
+                    // TODO : 실제 사거리 가져와야함!
+                    // 이미 사거리 안이라면 제자리
+                    if (distance <= _attackRange)
+                        targetPos = transform.position;
+                    else
+                        targetPos = monsterPos - dir * _attackRange;
+                }
+                // 맵일 경우
+                else
+                {
+                    _isTargetOn = false;
+                    _targetMonster = null;
+
+                    targetPos = hit.point;
+                }
+
+                if (NavMesh.SamplePosition(targetPos, out NavMeshHit navHit, 2.0f, NavMesh.AllAreas))
                 {
                     _agent.SetDestination(navHit.position);
                     State = CreatureState.Moving;
