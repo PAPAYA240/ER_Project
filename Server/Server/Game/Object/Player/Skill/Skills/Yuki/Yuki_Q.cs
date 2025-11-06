5cb97a5150df985807682b0a4e9c7f079772117f
junsu8282 <ghkdlxmejtk@naver.com>
2025-11-06 09:55:30 +0900

    애니메이션 추가 + 유키 R 수정

--- Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_Q.cs
+++ Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_Q.cs
@@ -10,16 +10,12 @@ public sealed class Yuki_Q : SkillHandlerBase
     public Yuki_Q()
     {
         _characterType = CharacterType.Yuki;
-        _animName = "SKILL_Q";
-        _keyCode = KeyCode.W;
+        //_animName = "SKILL_Q";
+        _keyCode = KeyCode.Q;
     }
 
     public override void OnEnter(Player p, SkillContext ctx)
     {
-        base.OnEnter(p, ctx);
-        // TODO: 코스트/쿨타임 차감
 
-        p.SendSkillConfirmPacket(true, ctx.Key, VariantKey.NoCollision);
-        p.LookAtMouse(ctx.MousePos);
     }
 }
