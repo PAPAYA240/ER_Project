83d0fffca317992b932ecde11edd30b45cf5ccb8
junsu8282 <ghkdlxmejtk@naver.com>
2025-11-07 18:57:20 +0900

    유키 Q 스킬 구현 + 타겟 회전 추가

--- Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_Q.cs
+++ Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_Q.cs
@@ -5,19 +5,18 @@ using System.Collections.Generic;
 using System.Text;
 using static Server.Data.DataUtils;
 
-public sealed class Yuki_Q : SkillHandlerBase
+public sealed class Yuki_Q : InstantHandlerBase
 {
     public Yuki_Q()
     {
         _characterType = CharacterType.Yuki;
-        //_animName = "SKILL_Q";
+        _animName = "SKILL_Q";
         _keyCode = KeyCode.Q;
     }
 
-    public override void OnEnter(Player p, SkillContext ctx)
+    public override void ExecuteInstant(Player p)
     {
-
-        SendSkillConfirmPacket(p);
-        p.LookAtMouse(ctx.MousePos);
+        p.AttackActive = true;
+        Console.WriteLine("Yuki Q Active");
     }
 }
