07cd00c7a96de59e225d99064310798c05d76fe9
kugiri <kyschaos@naver.com>
2025-11-13 10:29:29 +0900

    버그 수정

---
+++ Server/Server/Game/Object/Player/Skill/Skills/Hyunwoo/Hyunwoo_D.cs
@@ -0,0 +1,41 @@
+﻿using Google.Protobuf.Protocol;
+using Server.Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Server.Data.DataUtils;
+
+public sealed class Hyunwoo_D : SkillHandlerBase
+{
+    public Hyunwoo_D()
+    {
+        _characterType = CharacterType.Hyunwoo;
+        _animName = "SKILL_D";
+        _keyCode = KeyCode.D;
+    }
+
+    public override void OnEnter(Player p, SkillContext ctx)
+    {
+        base.OnEnter(p, ctx);
+
+        SendSkillConfirmPacket(p);
+        p.LookAtMouse(ctx.MousePos);
+    }
+
+    public override void OnHit(Player p, SkillContext ctx)
+    {
+
+    }
+
+    public override void OnTick(Player p, SkillContext ctx)
+    {
+
+    }
+
+    public override void OnExit(Player p, SkillContext ctx)
+    {
+        base.OnExit(p, ctx);
+
+    }
+}
+
