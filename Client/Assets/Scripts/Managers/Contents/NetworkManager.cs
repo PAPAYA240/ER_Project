3b0ee275e2c16ec5259789c8f8d26ce8cf77fc43
PAPAYA240 <zoehw10101@gmail.com>
2025-11-17 14:09:01 +0900

    commit

--- Client/Assets/Scripts/Managers/Contents/NetworkManager.cs
+++ Client/Assets/Scripts/Managers/Contents/NetworkManager.cs
@@ -18,11 +18,12 @@ public class NetworkManager
 
 	public void Init()
 	{
-		// DNS (Domain Name System)
-		//string host = Dns.GetHostName();
+        // DNS (Domain Name System)
+        //string host = Dns.GetHostName();
         //IPHostEntry ipHost = Dns.GetHostEntry(host);
         //IPAddress ipAddr = ipHost.AddressList[0];
-        IPAddress ipAddr = IPAddress.Parse("129.154.59.53"); // Public IP
+        IPAddress ipAddr = IPAddress.Loopback;
+        //IPAddress ipAddr = IPAddress.Parse("129.154.59.53"); // Public IP
         IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
 		Connector connector = new Connector();
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -40,9 +40,9 @@ namespace Server
             RoomManager.Instance.AddGameRoom(1);
 			
             // DNS (Domain Name System)
-            //string host = Dns.GetHostName();
-			//IPHostEntry ipHost = Dns.GetHostEntry(host);
-			//IPAddress ipAddr = ipHost.AddressList[0];
+            string host = Dns.GetHostName();
+			IPHostEntry ipHost = Dns.GetHostEntry(host);
+			IPAddress ipAddr = ipHost.AddressList[0];
 			//IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7777);
