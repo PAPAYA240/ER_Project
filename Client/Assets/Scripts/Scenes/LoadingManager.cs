21c8ae721fd3430375c8fdcc6f30605ff3eadb95
junsu8282 <ghkdlxmejtk@naver.com>
2025-11-20 18:17:49 +0900

    로딩바 추가

--- Client/Assets/Scripts/Scenes/LoadingManager.cs
+++ Client/Assets/Scripts/Scenes/LoadingManager.cs
@@ -21,48 +21,37 @@ public class LoadingManager : MonoBehaviour
         }
     }
 
-    public string GetSceneName(Define.Scene type)
-    {
-        string name = System.Enum.GetName(typeof(Define.Scene), type);
-        return name;
-    }
-
     public void LoadScene(Define.Scene type)
     {
-        nextScene = GetSceneName(type);
-        SceneManager.LoadScene("Loading");  // 로딩씬으로 이동
+        nextScene = System.Enum.GetName(typeof(Define.Scene), type);
+
+        // 로딩씬을 먼저 열기
+        SceneManager.LoadScene("Loading");
+
+        // 다음 프레임에 로딩 프로세스 시작
+        StartCoroutine(LoadSceneProcess());
     }
 
     public IEnumerator LoadSceneProcess()
     {
+        yield return null;
+
         AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
         op.allowSceneActivation = false;
 
-        float timer = 0f;
-
         while (!op.isDone)
         {
-            if (op.progress < 0.9f)
-            {
-                // 0 ~ 0.9 구간
-                //LoadingUIController.Instance.SetProgress(op.progress);
-                Debug.Log("Loading: " + (op.progress * 100f));
-            }
-            else
-            {
-                // 0.9 ~ 1.0 구간 (보간)
-                timer += Time.deltaTime;
-                float progress = Mathf.Lerp(0.9f, 1f, timer);
-
-                //LoadingUIController.Instance.SetProgress(progress);
-                Debug.Log("Loading: " + (progress * 100f));
-                if (progress >= 1f)
-                {
-                    op.allowSceneActivation = true;
-                }
-            }
-
             yield return null;
+
+            float ProgressValue = Mathf.Clamp01(op.progress / 0.9f);
+            LoadingUIController.Instance?.SetProgress(op.progress);
+
+            if (op.progress >= 0.9f)
+            {
+                LoadingUIController.Instance?.SetProgress(1f);
+                op.allowSceneActivation = true;
+                yield break;
+            }
         }
     }
 }
