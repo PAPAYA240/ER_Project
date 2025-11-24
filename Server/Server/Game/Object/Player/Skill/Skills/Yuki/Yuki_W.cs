32b378974eea06de9ae73ce622aacf4e6ebd814d
PAPAYA240 <zoehw10101@gmail.com>
2025-11-25 03:06:22 +0900

    commit

--- Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_W.cs
+++ Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_W.cs
@@ -41,7 +41,7 @@ public sealed class Yuki_W : SkillHandlerBase
         p.YukiStud = 4;
         Console.WriteLine($"유키 단추 두번들어오나? : {p.YukiStud}");
 
-        S_Yukistud yukiStudPkt = new S_Yukistud();
+        S_YukiStud yukiStudPkt = new S_YukiStud();
         yukiStudPkt.ObjectId = p.Id;
         yukiStudPkt.StudCnt = p.YukiStud;
 
