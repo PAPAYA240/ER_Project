c9f3002e0ae2fa2c6eb5d33456c1aaafbf14e435
yeonjin <xxyeon17@gmail.com>
2025-12-01 10:26:22 +0900

    머지

--- Client/Assets/Scripts/Packet/ClientPacketManager.cs
+++ Client/Assets/Scripts/Packet/ClientPacketManager.cs
@@ -178,7 +178,9 @@ class PacketManager
 		_onRecv.Add((ushort)MsgId.SPickSound, MakePacket<S_PickSound>);
 		_handler.Add((ushort)MsgId.SPickSound, PacketHandler.S_PickSoundHandler);		
 		_onRecv.Add((ushort)MsgId.SRemoveEffect, MakePacket<S_RemoveEffect>);
-		_handler.Add((ushort)MsgId.SRemoveEffect, PacketHandler.S_RemoveEffectHandler);
+		_handler.Add((ushort)MsgId.SRemoveEffect, PacketHandler.S_RemoveEffectHandler);		
+		_onRecv.Add((ushort)MsgId.SRozziNormalAttack, MakePacket<S_RozziNormalAttack>);
+		_handler.Add((ushort)MsgId.SRozziNormalAttack, PacketHandler.S_RozziNormalAttackHandler);
 	}
 
 	public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer)
