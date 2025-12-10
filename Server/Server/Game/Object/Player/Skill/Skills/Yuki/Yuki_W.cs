2496c168eb12a717c95915c384493790e3b8c22e
yeonjin <xxyeon17@gmail.com>
2025-12-11 03:16:48 +0900

    Update Yuki_W.cs

--- Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_W.cs
+++ Server/Server/Game/Object/Player/Skill/Skills/Yuki/Yuki_W.cs
@@ -6,22 +6,34 @@ using static Server.Data.DataUtils;
 
 public sealed class Yuki_W : SkillHandlerBase
 {
+    public override bool CanMoveDuringCast => true;
+
     private bool _hasSentRunAnimation;
 
     private string ANIM_RUN = "RUN";
     private string ANIM_IDLE = "WAIT";
+    private string ANIM_SKILL = "SKILL_W";
 
     public Yuki_W()
     {
         _characterType = CharacterType.Yuki;
-        _animName = "YUKI_W";
+        _animName = ANIM_SKILL;
         _keyCode = KeyCode.W;
     }
 
     public override void OnEnter(Player p, SkillContext ctx)
     {
-        base.OnEnter(p, ctx);
-        // TODO: 코스트/쿨타임 차감
+        // 전투 모드
+        {
+            p.CombatState = CombatState.Combat;
+            S_CombatMode combatModePkt = new S_CombatMode();
+            combatModePkt.CombatMode = p.CombatState;
+            p.Room.Push(p.Session.Send, combatModePkt);
+            p.CombatTime = 0f;
+        }
+
+        // 애니메이션 패킷 전송
+        p.SendAnimPacket("YUKI_W", 0.05f);
 
         SendSkillConfirmPacket(p);
 
